61d978e18e8a0bb02e4ebc3d651aeaa2ce359c52
1657 ADITYAPUSHP <22051657@kiit.ac.in>
2025-12-18 00:25:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7351490864-5f353",
+  "appId": "1:918753015518:web:aa6d3f061ceb7b8108b44f",
+  "apiKey": "AIzaSyC6CFjGLHJVsfCDyGQWWzZ4T_9kkPGLKqY",
+  "authDomain": "studio-7351490864-5f353.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "918753015518"
+};
